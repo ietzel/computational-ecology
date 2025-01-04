1ae3a6509a5416adf5afb473a90544338b2ef7cb
ietzel <67884277+ietzel@users.noreply.github.com>
2025-01-04 18:27:38 -0500

    Update main.rs

--- main.rs
+++ main.rs
@@ -2,5 +2,5 @@ fn main() {
   println!("Land: Desert, Plains, Forest, Valley, Mountains, Tundra, Caves.");
   println!("Water: Rivers, Lakes, Coasts, Wetlands, Deep Sea.");
   println!("Other: Rock Formations, Weather, Atmospheric Optical Phenomenona, Astronomy.");
-  println!(Take note of the ecological forms, filling in details of the ecological processes contained in each space.);
+  println!("Take note of the ecological forms, filling in details of the ecological processes contained in each space.");
 }

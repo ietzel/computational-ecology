306dffb68cd5b0a89bcba2729ecb10d5da12f460
ietzel <67884277+ietzel@users.noreply.github.com>
2025-01-04 18:36:07 -0500

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,4 +1,4 @@
 fn main() {
-  println!("Here are forms by elevation desc: Deep Sea, Lakes, Rivers, Coasts, Wetlands, Desert, Plains, Forest, Caves, Tundra, Valley, Mountains, Rock Formations, Weather, Atmospheric Optical Phenomenona, Astronomy.");
+  println!("Here are forms by elevation asc: Deep Sea, Lakes, Rivers, Coasts, Wetlands, Desert, Plains, Forest, Caves, Tundra, Valley, Mountains, Rock Formations, Weather, Atmospheric Optical Phenomenona, Astronomy.");
   println!("Take note of ecological forms, filling in events.");
 }

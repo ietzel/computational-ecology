4d1c6b525622ccfdf6ab2fe1d03224be8a39a7c7
ietzel <67884277+ietzel@users.noreply.github.com>
2025-01-05 15:38:58 -0500

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,4 +1,4 @@
 fn main() {
   println!("Here are forms by elevation asc: Deep Sea, Lakes, Rivers, Coasts, Wetlands, Desert, Plains, Forest, Caves, Tundra, Valley, Mountains, Rock Formations, Weather, Atmospheric Optical Phenomenona, Astronomy.");
-  println!("Take note of ecological forms, filling in succession of events such as the following: fires, floods, droughts, windstorms, landslides, avalanches, volcanic eruptions, disease epidemics, ocean temperature changes.");
+  println!("Take note of succession of events such as the following: fires, floods, droughts, windstorms, landslides, avalanches, volcanic eruptions, disease epidemics, ocean temperature changes.");
 }

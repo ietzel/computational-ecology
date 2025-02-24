ed7ed9c8c581b1d4215d1f266e803a7dd0743fbb
ietzel <67884277+ietzel@users.noreply.github.com>
2025-02-24 17:53:14 -0500

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,4 +1,4 @@
 fn main() {
-  println!("Here are forms by elevation asc: Deep Sea, Lakes, Rivers, Coasts, Wetlands, Desert, Plains, Forest, Caves, Tundra, Valley, Mountains, Rock Formations, Weather, Atmospheric Optical Phenomenona, Astronomy.");
+  println!("Here are forms by elevation asc (and their habitation rating from 0.0 to 10.0): Deep Sea (5.9), Lakes (6.5), Rivers (7.1), Coasts (7.8), Wetlands (4.1), Desert (2.9), Plains (9.0), Forest (9.6), Caves (4.7), Tundra (2.3), Valley (8.4), Mountains (5.3), Rock Formations (3.5), Weather (1.7), Atmospheric Optical Phenomenona (0.5), Astronomy (1.1).");
   println!("Take note of succession of events such as the following: fires, floods, droughts, windstorms, landslides, avalanches, volcanic eruptions, disease epidemics, ocean temperature changes.");
 }

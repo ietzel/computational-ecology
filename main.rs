718e62695d5dbd8f891dc3222fd76572666e507a
ietzel <67884277+ietzel@users.noreply.github.com>
2025-02-28 13:00:12 -0500

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,8 +1,8 @@
 fn main() {
-  let formsA: [f32; 16] = [Deep Sea (5.9), Lakes (6.5), Rivers (7.1), Coasts (7.7), Wetlands (4.1), Desert (2.9), Plains (8.9), Forest (9.5), Caves (4.7), Tundra (2.3), Valley (8.3), Mountains (5.3), Rock Formations (3.5), Weather (1.7), Atmospheric Optical Phenomenona (0.5), Astronomy (1.1)];
-  let formsB: [f32; 16] = [];
-  let successionsA: [f32; 16] = [fires (2.0), floods (5.0), droughts (3.0), windstorms (6.0), landslides (7.0), avalanches (9.0), volcanic eruptions (1.0), disease epidemics (8.0), ocean temperature changes (4.0)];
-  let successionsB: [f32; 16] = [];
+  let formsA: [f32; 16] = ["Deep Sea" (), "Lakes" (), "Rivers" (), "Coasts" (), "Wetlands" (), "Desert" (), "Plains" (), "Forest" (), "Caves" (), "Tundra" (), "Valley" (), "Mountains" (), "Rock Formations" (), "Weather" (), "Atmospheric Optical Phenomenona" (), "Astronomy" ()];
+  let formsB: [f32; 16] = [5.9, 6.5, 7.1, 7.7, 4.1, 2.9, 8.9, 9.5, 4.7, 2.3, 8.3, 5.3, 3.5, 1.7, 0.5, 1.1];
+  let successionsA: [f32; 16] = ["fires" (), "floods" (), "droughts" (), "windstorms" (), "landslides" (), "avalanches" (), "volcanic eruptions" (), "disease epidemics" (), "ocean temperature changes" ()];
+  let successionsB: [f32; 16] = [2.0, 5.0, 3.0, 6.0, 7.0, 9.0, 1.0, 8.0, 4.0];
   println!("Here are forms by elevation asc (and their habitation rating from 0.0 to 10.0):");
   println!("Take note of succession of events such as the following:");
 }

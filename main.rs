69fd16c3d75fbcb2902fd95414eacfd223225e2a
ietzel <67884277+ietzel@users.noreply.github.com>
2025-02-28 13:01:52 -0500

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,8 +1,10 @@
 fn main() {
   let formsA: [f32; 16] = ["Deep Sea" (), "Lakes" (), "Rivers" (), "Coasts" (), "Wetlands" (), "Desert" (), "Plains" (), "Forest" (), "Caves" (), "Tundra" (), "Valley" (), "Mountains" (), "Rock Formations" (), "Weather" (), "Atmospheric Optical Phenomenona" (), "Astronomy" ()];
   let formsB: [f32; 16] = [5.9, 6.5, 7.1, 7.7, 4.1, 2.9, 8.9, 9.5, 4.7, 2.3, 8.3, 5.3, 3.5, 1.7, 0.5, 1.1];
+  let mut formsC [f32; 16] = [];
   let successionsA: [f32; 16] = ["fires" (), "floods" (), "droughts" (), "windstorms" (), "landslides" (), "avalanches" (), "volcanic eruptions" (), "disease epidemics" (), "ocean temperature changes" ()];
   let successionsB: [f32; 16] = [2.0, 5.0, 3.0, 6.0, 7.0, 9.0, 1.0, 8.0, 4.0];
+  let mut successionsC [f32; 16] = [];
   println!("Here are forms by elevation asc (and their habitation rating from 0.0 to 10.0):");
   println!("Take note of succession of events such as the following:");
 }

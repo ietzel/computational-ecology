726265d73cd5b7dc6dddf1ad5e016d22b520f0bf
ietzel <67884277+ietzel@users.noreply.github.com>
2025-02-28 12:47:00 -0500

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,4 +1,8 @@
 fn main() {
+  let formsA: [f32; 16] = [];
+  let formsB: [f32; 16] = [];
+  let successionsA: [f32; 16] = [];
+  let successionsB: [f32; 16] = [];
   println!("Here are forms by elevation asc (and their habitation rating from 0.0 to 10.0): Deep Sea (5.9), Lakes (6.5), Rivers (7.1), Coasts (7.7), Wetlands (4.1), Desert (2.9), Plains (8.9), Forest (9.5), Caves (4.7), Tundra (2.3), Valley (8.3), Mountains (5.3), Rock Formations (3.5), Weather (1.7), Atmospheric Optical Phenomenona (0.5), Astronomy (1.1).");
   println!("Take note of succession of events such as the following: fires (2.0), floods (5.0), droughts (3.0), windstorms (6.0), landslides (7.0), avalanches (9.0), volcanic eruptions (1.0), disease epidemics (8.0), ocean temperature changes (4.0).");
 }
